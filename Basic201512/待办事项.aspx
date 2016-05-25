0c2d19c99f133ffeb3c72a80867968d8ab4f64e8
LaiChao <804365828@qq.com>
2016-05-25 17:16:53 +0800

    权限越低，显示的条目越少

--- Basic201512/待办事项.aspx
+++ Basic201512/待办事项.aspx
@@ -17,22 +17,44 @@
         </h2>
     </div>
     <div>
-        <a href="信息接收.aspx" target="main" >查看未读信息</a>
-        <asp:Label ID="Label1" runat="server" ForeColor="Red" Text="（0）"></asp:Label>
-        <br /><br />
-        <a href="../Permission/用户权限.aspx" target="main" >给新用户分配权限</a>
-        <asp:Label ID="Label2" runat="server" ForeColor="Red" Text="（0）"></asp:Label>
-        <br /><br />
-        <a href="项目审批.aspx" target="main" >审批项目</a>
-        <asp:Label ID="Label3" runat="server" ForeColor="Red" Text="（0）"></asp:Label>
-        <br /><br />
-        <a href="资金录入.aspx" target="main" >确认捐赠金额</a>
-        <asp:Label ID="Label4" runat="server" ForeColor="Red" Text="（0）"></asp:Label>
-        <br /><br />
-        <a href="捐赠人信息管理.aspx" target="main" >冠名慈善捐助金到期</a>
-        <asp:Label ID="Label5" runat="server" ForeColor="Red" Text="（0）"></asp:Label>
-        <br /><br />
 
+        <table runat="server">
+            <tr id="tr1">
+                <td>
+                <p>
+                <a href="信息接收.aspx" target="main" >查看未读信息</a>
+                <asp:Label ID="Label1" runat="server" ForeColor="Red" Text="（0）"></asp:Label>
+                </p></td>
+            </tr>
+            <tr id="tr2">
+                <td>
+                <p>
+                <a href="用户权限.aspx" target="main" >给新用户分配权限</a>
+                <asp:Label ID="Label2" runat="server" ForeColor="Red" Text="（0）"></asp:Label>
+                </p></td>
+            </tr>
+            <tr id="tr3">
+                <td>
+                <p>
+                <a href="项目审批.aspx" target="main" >审批项目</a>
+                <asp:Label ID="Label3" runat="server" ForeColor="Red" Text="（0）"></asp:Label>
+                </p></td>
+            </tr>
+            <tr id="tr4">
+                <td>
+                <p>
+                <a href="资金录入.aspx" target="main" >确认捐赠金额</a>
+                <asp:Label ID="Label4" runat="server" ForeColor="Red" Text="（0）"></asp:Label>
+                </p></td>
+            </tr>
+            <tr id="tr5">
+                <td>
+                <p>
+                <a href="捐赠人信息管理.aspx" target="main" >冠名慈善捐助金到期</a>
+                <asp:Label ID="Label5" runat="server" ForeColor="Red" Text="（0）"></asp:Label>
+                </p></td>
+            </tr>
+        </table>
     </div>
     </form>
     </center>
--- Basic201512/待办事项.aspx.cs
+++ Basic201512/待办事项.aspx.cs
@@ -16,6 +16,8 @@ public partial class Basic201512_待办事项 : System.Web.UI.Page
     {
         if(!Page.IsPostBack)
         {
+            int intRole = Convert.ToInt32(Session["userRole"].ToString());
+            
             //查看未读信息
             StringBuilder infoQuery = new StringBuilder();
             infoQuery.Append("select count(*) from e_info where infoRead='未读' ");
@@ -31,19 +33,39 @@ public partial class Basic201512_待办事项 : System.Web.UI.Page
             while (mysqlread.Read())
                 Label1.Text = "（" + mysqlread.GetString(0) + "）";
 
-            //分配权限
-            MySqlDataReader mysqlread1 = msq.getmysqlread("select count(*) from e_user where userRole is null");
-            while (mysqlread1.Read())
-                Label2.Text = "（" + mysqlread1.GetString(0) + "）";
-            //申请的项目
-            MySqlDataReader mysqlread2 = msq.getmysqlread("select count(*) from e_project where proschedule='申请'");
-            while (mysqlread2.Read())
-                Label3.Text = "（" + mysqlread2.GetString(0) + "）";
+            if(intRole==4)
+            {//分配权限
+                MySqlDataReader mysqlread1 = msq.getmysqlread("select count(*) from e_user where userRole is null");
+                while (mysqlread1.Read())
+                    Label2.Text = "（" + mysqlread1.GetString(0) + "）";
+            }
+            else
+            {
+                tr2.Visible = false;
+            }
+            
+            if(intRole==1)
+            {
+                tr3.Visible = false;
+            }
+            else
+            { //申请的项目
+                MySqlDataReader mysqlread2 = msq.getmysqlread("select count(*) from e_project where proschedule='申请'");
+                while (mysqlread2.Read())
+                    Label3.Text = "（" + mysqlread2.GetString(0) + "）";
+            }
 
-            //确认添加的资金
-            MySqlDataReader mysqlread3 = msq.getmysqlread("select count(*) from e_capital where state=0");
-            while (mysqlread3.Read())
-                Label4.Text = "（" + mysqlread3.GetString(0) + "）";
+            if (intRole == 1)
+            {
+                tr4.Visible = false;
+            }
+            else
+            {
+                //确认添加的资金
+                MySqlDataReader mysqlread3 = msq.getmysqlread("select count(*) from e_capital where state=0");
+                while (mysqlread3.Read())
+                    Label4.Text = "（" + mysqlread3.GetString(0) + "）";
+            }
 
             //冠名捐助金到期日期
             MySqlDataReader mysqlread4 = msq.getmysqlread("select count(*) from e_benfactor where deadline<now()");
