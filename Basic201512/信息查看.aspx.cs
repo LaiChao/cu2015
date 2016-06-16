ec90f41f8210a67c38ae7d8c6a35870b536e1186
LaiChao <804365828@qq.com>
2016-06-16 17:29:16 +0800

    注释无用代码

--- Basic201512/信息查看.aspx.cs
+++ Basic201512/信息查看.aspx.cs
@@ -29,9 +29,9 @@ public partial class Basic201512_信息查看 : System.Web.UI.Page
 {
 
     #region "自定义属性"
-    internal static Manager manager = Managers.Members["NewUtilityOra"] as Manager;
-    internal static ISingleTable entityUsers = manager.Entities["USERS"] as ISingleTable;
-    private DataTable dtUsers;
+    //internal static Manager manager = Managers.Members["NewUtilityOra"] as Manager;
+    //internal static ISingleTable entityUsers = manager.Entities["USERS"] as ISingleTable;
+    //private DataTable dtUsers;
 
     private string bindPCKey = "USER_ID";//绑定 能源节点数据的主键
 
