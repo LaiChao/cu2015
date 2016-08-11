9fc3877135dd758cc7764e995fc58bf1dde2b10a
LaiChao <804365828@qq.com>
2016-08-11 08:23:38 +0800

    分会也可以查看项目审批页面，但没有审批权限

--- Basic201512/项目审批.aspx.cs
+++ Basic201512/项目审批.aspx.cs
@@ -9,6 +9,7 @@ using System.Web.SessionState;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.HtmlControls;
+using System.Text;
 
 //使用数据访问层添加的必备引用
 using DataEntity.EntityManager;
@@ -24,7 +25,6 @@ using MySql.Data.MySqlClient;
 public partial class Basic201512_受助人 : System.Web.UI.Page
 {
     mysqlconn msq=new mysqlconn();
-    string str111 = string.Format("select * from e_project where proschedule='申请中' or proschedule='执行' order by proschedule desc");//shenpi2<>'1' or shenpi2 is null and shenpi1<>'1' or shenpi1 is null
     protected void Page_Load(object sender, EventArgs e)
     {
         //DataSet ds1 = MySqlHelper.ExecuteDataset(msq.getmysqlcon(),str111);
@@ -32,7 +32,14 @@ public partial class Basic201512_受助人 : System.Web.UI.Page
         //dgHeader.DataSource = dv1;
         //dgHeader.DataBind();
       //  ((BoundColumn)dgData.Columns[0]).HeaderText="编辑";
-        DataSet dds = MySqlHelper.ExecuteDataset(msq.getmysqlcon(),str111);
+        StringBuilder queryString = new StringBuilder();
+        queryString.Append("select * from e_project where (proschedule='申请中' or proschedule='执行') ");
+        if(Session["userRole"].ToString()=="1")
+            queryString.Append("and benfactorFrom='" + Session["benfactorFrom"].ToString() + "' ");
+        queryString.Append("order by proschedule desc");
+        //string str111 = string.Format("select * from e_project where proschedule='申请中' or proschedule='执行' order by proschedule desc");//shenpi2<>'1' or shenpi2 is null and shenpi1<>'1' or shenpi1 is null
+
+        DataSet dds = MySqlHelper.ExecuteDataset(msq.getmysqlcon(),queryString.ToString());
         DataView ddv = new DataView(dds.Tables[0]);
         dgData.DataSource = dds;
         dgData.DataBind();
@@ -41,8 +48,13 @@ public partial class Basic201512_受助人 : System.Web.UI.Page
    
     protected void Btselect_Click(object sender, EventArgs e)
     {
-        string str = string.Format("select * from e_project where projectID='{0}'or projectName='{1}'",TbselectID.Text,TbselectName.Text);
-        DataSet ds = MySqlHelper.ExecuteDataset(msq.getmysqlcon(),str);
+        string str = string.Format("select * from e_project where (projectID='{0}'or projectName='{1}') ",TbselectID.Text,TbselectName.Text);
+        StringBuilder queryString2 = new StringBuilder();
+        queryString2.Append(str);
+        if (Session["userRole"].ToString() == "1")
+            queryString2.Append("and benfactorFrom='" + Session["benfactorFrom"].ToString() + "' ");
+
+        DataSet ds = MySqlHelper.ExecuteDataset(msq.getmysqlcon(),queryString2.ToString());
         DataView dv = new DataView(ds.Tables[0]);
         dgData.DataSource = dv;
         dgData.DataBind();
